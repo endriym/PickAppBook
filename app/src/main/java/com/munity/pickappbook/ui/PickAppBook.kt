9db7b5f2d0da17c22cdde47886e44779fe0c22eb
endriym <endriymeraj10@hotmail.com>
2025-05-25 17:50:01 +0200

    Add missing Snackbar Host

--- app/src/main/java/com/munity/pickappbook/ui/PickAppBook.kt
+++ app/src/main/java/com/munity/pickappbook/ui/PickAppBook.kt
@@ -3,6 +3,7 @@ package com.munity.pickappbook.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
@@ -27,6 +28,7 @@ fun PickAppBook() {
                     onHomeButtonClick = { navHostController.navigateToHome() },
                 )
             },
+            snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
             modifier = Modifier.fillMaxSize(),
         ) { innerPadding ->
             PickAppBookNavHost(
