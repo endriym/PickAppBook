c389a6bbc0647c6223b7bb4e1be3c43728d510c2
endriym <endriymeraj10@hotmail.com>
2025-06-02 01:32:36 +0200

    ui: Add reusable PasswordTextField component

---
+++ app/src/main/java/com/munity/pickappbook/core/ui/components/PasswordTextField.kt
@@ -0,0 +1,47 @@
+package com.munity.pickappbook.core.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun PasswordTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var passwordHidden by remember { mutableStateOf(true) }
+    val passwordVisualTransformation = PasswordVisualTransformation()
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text(text = "Password") },
+        label = { Text(text = "Password") },
+        singleLine = true,
+        visualTransformation = if (passwordHidden) passwordVisualTransformation else VisualTransformation.None,
+        trailingIcon = {
+            if (value.isNotEmpty()) {
+                IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                    val visibilityIcon =
+                        if (passwordHidden) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                    val contentDescription =
+                        if (passwordHidden) "Show password" else "Hide password"
+                    Icon(imageVector = visibilityIcon, contentDescription = contentDescription)
+                }
+            }
+        },
+        modifier = modifier
+    )
+}
