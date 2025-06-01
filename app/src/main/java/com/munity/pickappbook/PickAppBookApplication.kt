aecf131d73a418c2c3921f3f18a07df4f71c9efe
endriym <endriymeraj10@hotmail.com>
2025-06-02 01:27:10 +0200

    Add manual dependency injection for ThePlaybookRepository

--- app/src/main/java/com/munity/pickappbook/PickAppBookApplication.kt
+++ app/src/main/java/com/munity/pickappbook/PickAppBookApplication.kt
@@ -6,6 +6,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.munity.pickappbook.core.data.local.datastore.PickAppPrefsDataSource
+import com.munity.pickappbook.core.data.remote.ThePlaybookDataSource
 import com.munity.pickappbook.core.data.repository.ThePlaybookRepository
 
 private const val DATA_STORE_PREFERENCES_NAME = "preferences"
@@ -15,12 +16,17 @@ private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
 
 class PickAppBookApplication : Application() {
     private lateinit var pickAppPrefsDataSource: PickAppPrefsDataSource
+    private lateinit var thePlaybookDataSource: ThePlaybookDataSource
     lateinit var thePlaybookRepository: ThePlaybookRepository
 
     override fun onCreate() {
         super.onCreate()
 
         pickAppPrefsDataSource = PickAppPrefsDataSource(dataStore = dataStore)
-        thePlaybookRepository = ThePlaybookRepository(pickAppPrefsDS = pickAppPrefsDataSource)
+        thePlaybookDataSource =
+            ThePlaybookDataSource(pickAppPrefsDataSource = pickAppPrefsDataSource)
+        thePlaybookRepository = ThePlaybookRepository(
+            pickAppPrefsDS = pickAppPrefsDataSource, thePlaybookDS = thePlaybookDataSource
+        )
     }
 }
