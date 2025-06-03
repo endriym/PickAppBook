aa472c0fcd79ca0e5ef90d5888325a99884fbde9
endriym <endriymeraj10@hotmail.com>
2025-06-03 04:40:45 +0200

    feat(reactions): Add update functionality for isStarred and vote, introduce repository coroutine scope

--- app/src/main/java/com/munity/pickappbook/PickAppBookApplication.kt
+++ app/src/main/java/com/munity/pickappbook/PickAppBookApplication.kt
@@ -8,6 +8,9 @@ import androidx.datastore.preferences.preferencesDataStore
 import com.munity.pickappbook.core.data.local.datastore.PickAppPrefsDataSource
 import com.munity.pickappbook.core.data.remote.ThePlaybookDataSource
 import com.munity.pickappbook.core.data.repository.ThePlaybookRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 
 private const val DATA_STORE_PREFERENCES_NAME = "preferences"
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
@@ -18,15 +21,26 @@ class PickAppBookApplication : Application() {
     private lateinit var pickAppPrefsDataSource: PickAppPrefsDataSource
     private lateinit var thePlaybookDataSource: ThePlaybookDataSource
     lateinit var thePlaybookRepository: ThePlaybookRepository
+    lateinit var applicationCoroutineScope: CoroutineScope
 
     override fun onCreate() {
         super.onCreate()
+
+        applicationCoroutineScope = CoroutineScope(context = Dispatchers.IO)
 
         pickAppPrefsDataSource = PickAppPrefsDataSource(dataStore = dataStore)
         thePlaybookDataSource =
             ThePlaybookDataSource(pickAppPrefsDataSource = pickAppPrefsDataSource)
         thePlaybookRepository = ThePlaybookRepository(
-            pickAppPrefsDS = pickAppPrefsDataSource, thePlaybookDS = thePlaybookDataSource
+            parentScope = applicationCoroutineScope,
+            pickAppPrefsDS = pickAppPrefsDataSource,
+            thePlaybookDS = thePlaybookDataSource
         )
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+
+        applicationCoroutineScope.cancel()
     }
 }
--- app/src/main/java/com/munity/pickappbook/core/data/repository/ThePlaybookRepository.kt
+++ app/src/main/java/com/munity/pickappbook/core/data/repository/ThePlaybookRepository.kt
@@ -1,21 +1,40 @@
 package com.munity.pickappbook.core.data.repository
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.munity.pickappbook.core.data.local.datastore.PickAppPrefsDataSource
 import com.munity.pickappbook.core.data.model.PickupLine
 import com.munity.pickappbook.core.data.model.User
 import com.munity.pickappbook.core.data.remote.ThePlaybookDataSource
+import com.munity.pickappbook.util.PickupLineUtil.plus
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
 
 class ThePlaybookRepository(
+    parentScope: CoroutineScope,
     private val pickAppPrefsDS: PickAppPrefsDataSource,
     private val thePlaybookDS: ThePlaybookDataSource,
 ) {
+    private val repositoryScope: CoroutineScope = run {
+        val supervisorJob = SupervisorJob(parent = parentScope.coroutineContext.job)
+        parentScope + Dispatchers.IO + supervisorJob
+    }
+
     val isLoggedIn: Flow<Boolean> = pickAppPrefsDS.storedPreference.map { storedPrefs ->
         !(storedPrefs.user.isNullOrEmpty() || storedPrefs.password.isNullOrEmpty())
     }
+
+    private val _pickupLines: SnapshotStateList<PickupLine> = mutableStateListOf<PickupLine>()
+    val pickupLines: List<PickupLine> = _pickupLines
 
     suspend fun login(username: String, password: String): String {
         val tokenInfo = thePlaybookDS.login(username, password)
@@ -46,14 +65,10 @@ class ThePlaybookRepository(
         val result = thePlaybookDS.createUser(user)
 
         val message: String = when {
-            result.isSuccess -> {
-                "Account successfully created for $username"
-            }
+            result.isSuccess -> "Account successfully created for $username"
 
-            result.isFailure -> {
-                result.exceptionOrNull()!!.message
-                    ?: "Something went wrong with the signup attempt"
-            }
+            result.isFailure ->
+                result.exceptionOrNull()!!.message ?: "Something went wrong with the signup attempt"
 
             else -> "Something went wrong with the signup attempt"
         }
@@ -61,20 +76,85 @@ class ThePlaybookRepository(
         return message
     }
 
-    suspend fun updatePickupLine(pickupLine: PickupLine): Result<PickupLine> {
-        return thePlaybookDS.updatePickupLine(pickupLine)
+    suspend fun updatePickupLine(pickupLine: PickupLine): Result<PickupLine> =
+        thePlaybookDS.updatePickupLine(pickupLine)
+
+    suspend fun getPickupLine(pickupLineId: String): Result<PickupLine> =
+        thePlaybookDS.getPickupLine(pickupLineId)
+
+    private suspend fun refreshPickupLine(pickupLineIndex: Int): Result<String> {
+        val result = getPickupLine(_pickupLines[pickupLineIndex].id)
+
+        return when {
+            result.isSuccess -> {
+                _pickupLines[pickupLineIndex] = result.getOrNull()!!
+                Result.success("PickupLine successfully updated")
+            }
+
+            else -> Result.failure(result.exceptionOrNull()!!)
+        }
     }
 
-    suspend fun updateReaction(
-        pickupLine: PickupLine,
+    private suspend fun updateReaction(
+        pickupLineIndex: Int,
         newReaction: PickupLine.Reaction,
+        oldReaction: PickupLine.Reaction,
     ): String {
-        val result = thePlaybookDS.putReaction(pickupLine.id, newReaction)
+        // Update affected pickup line
+        _pickupLines[pickupLineIndex] =
+            _pickupLines[pickupLineIndex].copy(reactions = listOf(newReaction))
+        val result = thePlaybookDS.putReaction(_pickupLines[pickupLineIndex].id, newReaction)
 
-        if (result.isSuccess)
+        if (result.isSuccess) {
+            // Get updated PickupLine
+            repositoryScope.launch {
+                refreshPickupLine(pickupLineIndex)
+            }
+
             return "Reactions successfully updated"
+        }
+
+        // Revert modified pickup line
+        _pickupLines[pickupLineIndex] =
+            _pickupLines[pickupLineIndex].copy(reactions = listOf(oldReaction))
+
+        // Get updated PickupLine
+        repositoryScope.launch {
+            refreshPickupLine(pickupLineIndex)
+        }
 
         return result.exceptionOrNull()!!.message ?: "Reaction not updated"
+    }
+
+    suspend fun updateStarred(pickupLineIndex: Int): String {
+        val oldReaction =
+            pickupLines[pickupLineIndex].reactions?.first()?.copy() ?: PickupLine.Reaction(
+                isStarred = false, vote = PickupLine.Vote.NONE.toString()
+            )
+
+        val newReaction = oldReaction.copy(isStarred = !oldReaction.isStarred)
+
+        return updateReaction(pickupLineIndex, newReaction, oldReaction)
+    }
+
+    suspend fun updateVote(pickupLineIndex: Int, newVote: PickupLine.Vote): String {
+        val oldReaction =
+            _pickupLines[pickupLineIndex].reactions?.first()?.copy() ?: PickupLine.Reaction(
+                isStarred = false, vote = PickupLine.Vote.NONE.toString()
+            )
+        val oldStatistics =
+            _pickupLines[pickupLineIndex].statistics?.copy() ?: PickupLine.Statistics(
+                0, 0, 0, 0.toFloat()
+            )
+
+        val (newVote, weights) = PickupLine.Vote.valueOf(oldReaction.vote).plus(newVote)
+        val newReaction = oldReaction.copy(vote = newVote.toString())
+
+        // Calculate new statistics with `weights`
+        _pickupLines[pickupLineIndex] =
+            _pickupLines[pickupLineIndex].copy(statistics = oldStatistics.plus(weights))
+
+        return updateReaction(pickupLineIndex, newReaction, oldReaction)
     }
 
     suspend fun getPickupLineList(
@@ -83,13 +163,24 @@ class ThePlaybookRepository(
         tagIds: List<String>? = null,
         visibility: Boolean? = null,
         content: String? = null,
-    ): Result<List<PickupLine>> = thePlaybookDS.getPickupLineList(
-        page = page,
-        title = title,
-        tagIds = tagIds,
-        isVisible = visibility,
-        content = content
-    ).map { it.pickupLines }
+    ): String? {
+        var message: String? = null
+
+        val result = thePlaybookDS.getPickupLineList(
+            page = page,
+            title = title,
+            tagIds = tagIds,
+            isVisible = visibility,
+            content = content
+        ).map { it.pickupLines }
+
+        when {
+            result.isSuccess -> _pickupLines.swapList(result.getOrNull()!!)
+            else -> message = result.exceptionOrNull()!!.message
+        }
+
+        return message
+    }
 
     suspend fun getPickupLineFeed(
         page: Int? = null,
@@ -97,11 +188,27 @@ class ThePlaybookRepository(
         tagIds: List<String>? = null,
         visibility: Boolean? = null,
         content: String? = null,
-    ): Result<List<PickupLine>> = thePlaybookDS.getPickupLineFeed(
-        page = page,
-        title = title,
-        tagIds = tagIds,
-        isVisible = visibility,
-        content = content
-    ).map { it.pickupLines }
+    ): String? {
+        var message: String? = null
+
+        val result = thePlaybookDS.getPickupLineFeed(
+            page = page,
+            title = title,
+            tagIds = tagIds,
+            isVisible = visibility,
+            content = content
+        ).map { it.pickupLines }
+
+        when {
+            result.isSuccess -> _pickupLines.swapList(result.getOrNull()!!)
+            else -> message = result.exceptionOrNull()!!.message
+        }
+
+        return message
+    }
+}
+
+fun <T> SnapshotStateList<T>.swapList(newList: List<T>) {
+    clear()
+    addAll(newList)
 }
