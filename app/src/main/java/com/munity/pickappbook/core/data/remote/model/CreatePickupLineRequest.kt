10f829fc7f9285d616ae6f53f289f6f201932afd
endriym <endriymeraj10@hotmail.com>
2025-07-11 18:54:52 +0200

    fix: add missing @Serializable annotation to CreatePickupLineRequest class

--- app/src/main/java/com/munity/pickappbook/core/data/remote/model/CreatePickupLineRequest.kt
+++ app/src/main/java/com/munity/pickappbook/core/data/remote/model/CreatePickupLineRequest.kt
@@ -1,5 +1,8 @@
 package com.munity.pickappbook.core.data.remote.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CreatePickupLineRequest(
     val title: String,
     val content: String,
