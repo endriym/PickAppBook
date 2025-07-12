318875b95b861f8762c93916d2468e17132776c3
endriym <endriymeraj10@hotmail.com>
2025-07-12 18:09:08 +0200

    feat: show a message when pickup line list is empty

--- app/src/main/java/com/munity/pickappbook/core/ui/components/LazyPickupCards.kt
+++ app/src/main/java/com/munity/pickappbook/core/ui/components/LazyPickupCards.kt
@@ -1,14 +1,22 @@
 package com.munity.pickappbook.core.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.munity.pickappbook.core.data.remote.model.PickupLineResponse
 import com.munity.pickappbook.core.data.remote.model.TagResponse
@@ -48,32 +56,50 @@ private fun LazyPickupCards(
     onTagClick: (TagResponse) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier.fillMaxSize()
-    ) {
-        itemsIndexed(
-            items = pickupLines, key = { index, pickupLine ->
-                pickupLine.id
-            }) { index, pickupLine ->
+    if (pickupLines.isNotEmpty()) {
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = modifier.fillMaxSize()
+        ) {
+            itemsIndexed(
+                items = pickupLines, key = { index, pickupLine ->
+                    pickupLine.id
+                }) { index, pickupLine ->
 
-            PickupCard(
-                authorImageUrl = pickupLine.userJpegImageUrl,
-                author = pickupLine.username,
-                postDate = DateUtil.iso8601ToTimeAgo(pickupLine.updatedAt),
-                titleLine = pickupLine.title,
-                line = pickupLine.content,
-                tags = pickupLine.tags,
-                reaction = pickupLine.reaction ?: PickupLineResponse.Reaction(
-                    isStarred = false,
-                    vote = PickupLineResponse.Vote.NONE
-                ),
-                statistics = pickupLine.statistics,
-                onStarredBtnClick = { onStarredBtnClick(index) },
-                onVoteClick = { vote -> onVoteClick(index, vote) },
-                onTagClick = onTagClick,
-                modifier = Modifier,
+                PickupCard(
+                    authorImageUrl = pickupLine.userJpegImageUrl,
+                    author = pickupLine.username,
+                    postDate = DateUtil.iso8601ToTimeAgo(pickupLine.updatedAt),
+                    titleLine = pickupLine.title,
+                    line = pickupLine.content,
+                    tags = pickupLine.tags,
+                    reaction = pickupLine.reaction ?: PickupLineResponse.Reaction(
+                        isStarred = false,
+                        vote = PickupLineResponse.Vote.NONE
+                    ),
+                    statistics = pickupLine.statistics,
+                    onStarredBtnClick = { onStarredBtnClick(index) },
+                    onVoteClick = { vote -> onVoteClick(index, vote) },
+                    onTagClick = onTagClick,
+                    modifier = Modifier,
+                )
+            }
+        }
+    } else {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "No pickup lines found",
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .padding(16.dp)
             )
         }
     }
